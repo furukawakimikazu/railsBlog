342dcbf18adbe9d1c9a615faffe015e2a2ca657c
furukawakimikazu <g.guquan@gmail.com>
2020-04-19 15:00:54 +0900

    create controller model

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,55 @@
+class ArticlesController < ApplicationController
+  before_action :find_article only: [:show, :edit, :update, ::destroy]
+  def index
+    @articles = Article.order(create_at: :desc)
+  end
+  
+
+  def show
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article, notice: '作成出来ました。'
+    else
+      render :new, alert: '作成できませんでした。'
+    end
+  end
+
+  def update
+    if @artile.update
+      redirect_to @article, notice: '更新出来ました。'
+    else
+      render :edit, alert: '更新できませんでした。'
+    end
+  end
+
+  def destroy
+    if @article.destroy
+      redirect_to root_path notice: '削除に成功しました。'
+    else
+      redirect_to root_path alert: '削除できませんでした。'
+    end
+  end
+
+    private
+      def find_article
+        @article = Article.find(params[:id])
+      end
+
+      def article_params
+        
+      end
+      
+
+  
+  
+end
