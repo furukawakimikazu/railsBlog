340d23edb2611a30eb65fd4eb89bf4f0b6919db4
furukawakimikazu <g.guquan@gmail.com>
2020-05-10 14:44:03 +0900

    users tableにfakerでフェイクデータを插入

--- Gemfile
+++ Gemfile
@@ -40,6 +40,7 @@ gem 'bootstrap-sass'
 gem 'devise'
 gem 'devise-i18n'
 gem 'devise-i18n-views'
+gem 'faker'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(
+  email: 'furukawa@netvision.tv',
+  password: 'password'
+)
+
+50.times do 
+  User.create(
+  email: Faker::Internet.email,
+  password: 'password'
+  )
+end
+
