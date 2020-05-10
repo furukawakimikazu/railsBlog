16bc238c7a9e9efbd02737990a765d4c5f8fdf4a
furukawakimikazu <g.guquan@gmail.com>
2020-05-10 10:47:31 +0900

    ログイン画面の作成とdevise-controllerの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   root 'home#top'
-  devise_for :users
   resources :articles
+
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'   
+  } 
+
+  devise_scope :user do
+    get "sign_in", :to => "users/sessions#new"
+    get "sign_out", :to => "users/sessions#destroy" 
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
